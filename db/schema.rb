f015ff37e4db490e965d16f2de0904ed9c326f0a
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2011-01-23 11:17:16 +0000

    Fixed some styling issues in cms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101225113828) do
+ActiveRecord::Schema.define(:version => 20110110211505) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -86,5 +93,25 @@ ActiveRecord::Schema.define(:version => 20101225113828) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "videos", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "embed"
+    t.string   "video_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "vtokens", :force => true do |t|
+    t.text     "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "perms"
+    t.string   "nsid"
+    t.string   "fullname"
+    t.string   "username"
+    t.string   "user_id"
+  end
 
 end
