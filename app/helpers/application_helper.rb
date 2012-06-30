328cd825632955e253b0dbdcd8ebf8f6b7fd38b7
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2012-06-30 11:50:50 +0100

    Twitter bootstrap styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,23 @@ module ApplicationHelper
   def published_on_text(resource)
     resource.published_on.strftime("%d %B, %Y")
   end
-  
+
   def comment_published_on_text(resource)
     resource.created_at.strftime("%d %B, %Y")
   end
-  
+
   def commentable_path(commentable)
     if commentable.class.name == "Post"
       post_path(commentable)
     elsif commentable.class.name == "Picture"
       photoalbum_picture_path(commentable.photoalbum, commentable)
     end
+  end
+
+  def header(header_text)
+    header = content_tag :div, :class => 'page-header' do
+      content_tag :h1, header_text
+    end
+    header
   end
 end
