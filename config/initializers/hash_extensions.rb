d57695f77ce1d310c645c511bf73d57ebba87869
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2012-07-01 22:39:17 +0100

    Extend hash for html attributes in bootstrap_helpers

---
+++ config/initializers/hash_extensions.rb
@@ -0,0 +1,12 @@
+class Hash
+
+  def merge_with_string_concat(hash)
+    result = self.merge(hash)
+    hash.each_pair do |key, value|
+      if self.has_key?(key) && self[key].is_a?(String) && value.is_a?(String)
+        result[key] = self[key].concat(" ").concat(value)
+      end
+    end
+    result
+  end
+end
