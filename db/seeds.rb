c7a2d613f3120d05adec5ddedd6764579b266022
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2012-10-27 19:42:40 +0100

    Fixup the seeds a bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,47 @@ themitchell = User.create!(
 
 personal_statement = ContentBlock.create!(
   name: "personal statement", 
-  body: "<p>some bunch  of text here</p>"
-)
+  body: "<p>I am a software developer/audio engineer based in London, Uk. I write mostly web applications" +
+        " using Ruby & Rails by day.</p>" +
+        "<p>I sometimes write web apps in node too using Socket.io and Express and I have a huge love for" +
+        " audio engineering and music as you can see!</p>" +
+        "<p>I also write, produce and mix electronic music.</p>"
+) unless ContentBlock.find_by_name("personal statement")
 
 unboxed = EmploymentHistoryItem.create!(
-  company_name:     "Unobxed Consulting",
-  company_role:     "Developer",
+  company_name:     "Unboxed Consulting",
+  company_role:     "Ruby / Front End Developer",
   role_description: "Developing web applications with Ruby, Ruby on Rails and Javascript. Behaviour & Test Driven Development and Agile methodolgies. On projects such Channel5.com, Reevoo.com, Agileista, Tipped and Contiki.co.uk",
   date_from:        "01/06/2010"
 ) unless EmploymentHistoryItem.find_by_company_name("Unboxed Consulting")
+
+freelance = EmploymentHistoryItem.create!(
+  company_name:     "Freelance PHP developer",
+  company_role:     "PHP Developer",
+  role_description: "Freelance development projects such as personal projects including Puzzled Records online presence and an online video portfolio for CeriAllen.com.",
+  date_from:        "01/01/2010",
+  date_to:          "01/06/2010"
+) unless EmploymentHistoryItem.find_by_company_name("Freelance PHP Developer")
+#  name        :string(255)
+#  institution :string(255)
+#  grade       :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#  date_from   :date
+#  date_to     :date
+
+masters = EducationalQualification.create!(
+  name:         'MSc Advanced Audio Production',
+  institution:  'University of Glamorgan',
+  grade:        'Merit',
+  date_from:    '01/09/2008',
+  date_to:      '01/10/2009'
+) unless EducationalQualification.find_by_name("MSc Advanced Audio Production")
+
+bachelors = EducationalQualification.create!(
+  name:         'BSc Audio Engineering',
+  institution:  'University of Glamorgan',
+  grade:        '2:1',
+  date_from:    '01/09/2005',
+  date_to:      '01/06/2008'
+) unless EducationalQualification.find_by_name("BSc Audio Engineering")
